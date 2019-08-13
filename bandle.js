6e0886182fc790c53f3f32d83ede1e9d9153751b
MashyToma <53348071+MashyToma@users.noreply.github.com>
2019-08-13 14:34:38 +0300

    Add files via upload

---
+++ bandle.js
@@ -0,0 +1,10 @@
+'use strict'
+var writeInThisWindow = prompt('Write in this window, please', '');
+
+if(writeInThisWindow =='null'){
+
+alert("It's not good...", )
+}else{  
+    alert("It's ok!")
+    
+}
