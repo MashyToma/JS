5145aef14991f7dde2000ce323f6208e96736efe
MashyToma <53348071+MashyToma@users.noreply.github.com>
2019-08-13 14:35:24 +0300

    Update bandle.js

--- bandle.js
+++ bandle.js
@@ -3,7 +3,7 @@ var writeInThisWindow = prompt('Write in this window, please', '');
 
 if(writeInThisWindow =='null'){
 
-alert("It's not good...", )
+alert("It's not good...")
 }else{  
     alert("It's ok!")
     
